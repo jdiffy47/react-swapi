d07b23f2749fbe698269c6721c1ed099b453c7a5
JakeD <jakediefenderfer@gmail.com>
2022-07-07 13:05:31 -0400

    complete starshipList

---
+++ src/pages/StarshipDetails/StarshipDetails.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { getDetails } from "../../services/sw-api";
+
+
+const StarshipDetails = () => {
+  return ( 
+    
+  )
+}
+ 
+export default StarshipDetails;
