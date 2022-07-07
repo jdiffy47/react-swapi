7570b049b2faf937470d6ce9ba8520db4b797f67
JakeD <jakediefenderfer@gmail.com>
2022-07-07 13:19:40 -0400

    stub up starship details page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import StarshipList from './pages/StarshipList/StarshipList';
+import StarshipDetails from './pages/StarshipDetails/StarshipDetails';
 
 function App() {
   return (
       <>
         <Routes>
           <Route path="/starship-list" element={<StarshipList />}
+          />
+          <Route path="/starship" element={<StarshipDetails />}
           />
         </Routes>
       </>
--- src/pages/StarshipDetails/StarshipDetails.jsx
+++ src/pages/StarshipDetails/StarshipDetails.jsx
@@ -4,9 +4,22 @@ import { getDetails } from "../../services/sw-api";
 
 
 const StarshipDetails = () => {
+  const [starshipDetails, setStarshipDetails] = useState({})
+  const location = useLocation()
+  useEffect(() => {
+    const fetchDetails = async () => 
+    {
+      const starshipDetails = await 
+      getDetails(location.state.starship.url)
+      setStarshipDetails(starshipDetails)
+    }
+    fetchDetails()
+  }, [location.state.starship.url])
+  
   return ( 
-    
+    <>
+      <h2>Starship deeds</h2>
+    </>
   )
 }
- 
 export default StarshipDetails;
